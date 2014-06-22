3291930336ab4cc9aaff75cecddae6b1a3631251
James Ruan <ruanbeihong@gmail.com>
2014-06-22 16:09:20 +0800

    update auth_access.ml.js

--- auth.ml.js
+++ auth.ml.js
@@ -6,6 +6,8 @@ API的授权使用\link+[OAuth2]{http://oauth.net/2/}。
 第三方应用采用\link+[Authorization Code Grant]{http://tools.ietf.org/html/rfc6749#section-4.1}，将在第二版API中实现。
 
 \h4{网站授权}
-由于Resource Owner Password Creditials是直接明文发送至服务器，服务器与客户端直接的连接必须是在HTTPS/TLS的保护下。
+网站使用OAuth2的\link+[Resource Owner Password Creditials Grant]{http://tools.ietf.org/html/rfc6749#section-4.3}方式授权，直接提交用户名和密码(Resource Owner Password Creditials)，服务器端验证身份后获得授权。由于Resource Owner Password Creditials是直接明文发送至服务器，服务器与客户端直接的连接必须是在HTTPS/TLS的保护下。
+
+授权后将获得一个用作访问的access token，和一个用于更新access token的refresh token；访问需要授权的资源时，在报头里面加入\@Authorization: Bearer {access token}\@，access token过期失效时，则用refresh token获取新的access token，避免再次使用用户名和密码。
 
 
--- auth_access.ml.js
+++ auth_access.ml.js
@@ -1,6 +1,8 @@
-\h3{Access Token Request}
+\h3{获取Access Token}
 
-内容实体包括6个参数：
+\h4{Access Token Request}
+
+\@application/x-www-form-urlencoded\@格式，内容实体包括6个参数：
 \list*{
     \* \@grant_type\@：必要，必须为\@password\@
     \* \@username\@：必要，用户的用户名
@@ -22,12 +24,12 @@ grant_type=password&username=johndoe&password=A3ddj3w&scope=api.cc98.org&client_
 
 服务器端需要
 \list#{
-    \* 是否满足限速条件（防止暴力破解密码）
+    \* 是否满足限速条件（防止暴力破解密码），具体参考\link+[限速]{#/ratelimit.ml.js}
     \* 验证client credentials，即\@client_id\@和\@client_secret\@是否合格
     \* 验证resource owner credentials，即\@username\@和\@password\@是否正确
 }
 
-\h4{成功回复}
+\h5{成功回复}
 
 如果通过了以上步骤，服务器发放access token和refresh token：
 
@@ -42,7 +44,7 @@ grant_type=password&username=johndoe&password=A3ddj3w&scope=api.cc98.org&client_
 
 \code+[http]{begin}
 HTTP/1.1 200 OK
-Content-Type: application/json; charset=UTF-8
+Content-Type: application/json;charset=UTF-8
 Cache-Control: no-cache, no-store
 \code+{end}
 
@@ -55,4 +57,54 @@ Cache-Control: no-cache, no-store
 }
 \code+{end}
 
+\h5{错误回复}
 
+参考：\link+[Error Response]{http://tools.ietf.org/html/rfc6749#section-5.2}
+
+错误回复应该使用400状态码，返回的参数包括
+\list*{
+    \* \@error\@：必须，错误状态，为以下几个之一：
+        \list*{
+            \* \@invalid_request\@：请求格式不正确，参数不正确
+            \* \@invalid_client\@：客户端认证失败
+            \* \@invalid_grant\@：token无效/过期
+            \* \@unauthorized_client\@：客户端不允许使用这个授权类型
+            \* \@unsupported_grant_type\@：授权类型不支持
+            \* \@invalid_scope\@：错误的scope
+        }
+    \* \@error_description\@：必须（OAuth2可选），human-readable ASCII字符，进一步描述错误类型，用于记录日志和开发调试用。
+    \* \@error_uri\@：必须（OAuth2可选），包含用户可读错误信息的页面地址，改页面内\emphasis{应该}包括引导用户完成正确授权的内容。
+}
+
+\code+[http]{begin}
+HTTP/1.1 400 Bad Request
+Content-Type: application/json;charset=UTF-8
+Cache-Control: no-store
+Pragma: no-cache
+\code+{end}
+
+\code+[json]{begin}
+{
+        "error":"invalid_request"
+}
+\code+{end}
+
+\h3{使用Access Token}
+
+Access token直接用于访问资源，仅仅需要在报头加入\@Authorization: Bearer {access_token}\@。
+
+\code+[http]{begin}
+GET /some_resource HTTP 1.1
+Host: api.cc98.org
+ContentType: application/json;charset=UTF-8
+Authorization: Bearer 2YotnFZFEjr1zCsicMWpAA
+\code+{end}
+
+服务器需要验证
+\list#{
+    \* 是否满足限速条件（防止脚本恶意刷贴），具体参考\link+[限速]{#/ratelimit.ml.js}
+    \* 验证access token是否正确，如果不正确，应引导用户重新登录；如果access token过期，应引导客户端刷新access token，参考\link+[refresh token]{#/auth_refresh.ml.js}
+    \* 访问的scope是否在授权内
+}
+
+错误回复参照上文。
