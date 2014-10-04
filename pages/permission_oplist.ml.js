ddf1b2ca5b3ad46a91009425f3392c98086a9e0d
James Ruan <ruanbeihong@gmail.com>
2014-10-04 11:59:21 +0800

    add LOG option to oplist

--- pages/permission_oplist.ml.js
+++ pages/permission_oplist.ml.js
@@ -11,7 +11,7 @@ Oplist是有层级的，最上层的oplist只能后台修改，API层面只读�
 op1: [cc98web] user
 op2: !user @group
 op3: $proxy
-!op1: !@group
+!op1: !@group (LOGDENIED)
 \code{end}
 
 Oplist根据op分为白名单和黑名单。
@@ -19,8 +19,8 @@ Oplist根据op分为白名单和黑名单。
 
 权限检查时按这样的逻辑进行：
 
-用户在白名单中，且用户不在黑名单中，则允许操作；\newline
-用户不在白名单中，或用户在黑名单中，则拒绝操作。
+用户在白名单中，\emphasis{且}用户不在黑名单中，则允许操作；\newline
+用户不在白名单中，\emphasis{或}用户在黑名单中，则拒绝操作。
 
 有oplist允许3类用户：
 \list*{
@@ -29,7 +29,7 @@ Oplist根据op分为白名单和黑名单。
     \* 代理用户：表示 \@$代理用户名\@
 }
 
-其中，用户名是固定的内容，用户组名代表了一系列固定的用户名，而代理用户名则代表了一系列不固定的用户名，根据oplist所对应的资源不同，代理用户名对应的用户也因此不同。
+其中，用户名是固定的内容，用户组名代表了一系列固定的用户名，而代理用户名则代表了一系列相对不固定的用户名，根据oplist所对应的资源不同，代理用户名对应的用户也不同。
 
 比如说，一个回复的oplist中，\@$author\@，就代表了这个回复的发表者，对于不同的回复，这个\@$author\@不同。
 
@@ -51,6 +51,14 @@ op1:@user
 
 用户列表前的\@[cc98web]\@表示API服务的客户端(client_id)，目前仅支持网页客户端\@cc98web\@，如果忽略，则表示默认的\@[cc98web]\@。
 
+每条oplist后的\@(LOGALLOWED)\@、\@(LOGDENIED)\@或\@(LOG)\@是可选标记（注意没有空格），如果存在，则做权限判断时，检查到这条oplist的动作会被记录下来。\@(LOGALLOWED)\@表示通过这条oplist的动作被记录下来；\@LOGDENIED\@则表示不通过时记录；\@LOG\@则为任何状态下都记录；如果没有这个标记，则表示不做记录；可以参考以下的格式记录：
+
+\code{begin}
+op1:user | YYYY-MM-DD HH:MM:SS.sss user doing op1 with operand xxx | ALLOWED
+op2:@group | YYYY-MM-DD HH:MM:SS.sss user@group doing op2 with operand xxx | DENIED
+!op3:$proxy | YYYY-MM-DD HH:MM:SS.sss user$proxy doing op3 with operand xxx | DENIED
+\code{end}
+
 完整语法定义为（in \link+[PEG.js]{http://pegjs.majda.cz/}）：
 
 \code+[haskell]{begin}
@@ -61,7 +69,9 @@ Oplist
 
 Items = Item+
 
-Item = (Whitelist / Blacklist) NL+
+Item = (Whitelist / Blacklist) Log_mark? NL+
+
+Log_mark = "(LOG" ("ALLOWED"/"DENIED")? ")"
 
 Whitelist = Op ":" Sp ("[" Sp? Client Sp?"]")? Users 
 
